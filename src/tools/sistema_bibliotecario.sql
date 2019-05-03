03d358e7d2462ebdb9b89f15846d082be38b0ec3
EstevenJaviier <estirastam.c@gmail.com>
2019-05-03 07:40:45 -0500

    update sql

--- src/tools/sistema_bibliotecario.sql
+++ src/tools/sistema_bibliotecario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2019 a las 21:45:25
+-- Tiempo de generación: 03-05-2019 a las 14:39:43
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sistema_bibliotecario`
+-- Base de datos: `sistema_blibliotecario`
 --
 
 -- --------------------------------------------------------
@@ -43,6 +43,84 @@ CREATE TABLE `funcionario` (
   `rol` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `material`
+--
+
+CREATE TABLE `material` (
+  `idMaterial` int(11) NOT NULL,
+  `codigoMaterial` varchar(50) NOT NULL,
+  `tipoMaterial` varchar(50) NOT NULL,
+  `autor` varchar(50) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  `editorial` date NOT NULL,
+  `edicion` varchar(50) NOT NULL,
+  `numPaginas` varchar(60) NOT NULL,
+  `tema` varchar(10) NOT NULL,
+  `disponible` varchar(50) NOT NULL,
+  `estadoFisico` varchar(50) NOT NULL,
+  `codigoAsignatura` varchar(50) NOT NULL,
+  `letraUbicacion` varchar(50) NOT NULL,
+  `Ejemplar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `multa`
+--
+
+CREATE TABLE `multa` (
+  `idMulta` int(11) NOT NULL,
+  `fechaMulta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `valorMulta` double NOT NULL,
+  `fechaPago` date NOT NULL,
+  `motivoMulta` text NOT NULL,
+  `controlPago` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamo`
+--
+
+CREATE TABLE `prestamo` (
+  `idPrestamo` int(11) NOT NULL,
+  `fechaPrestamo` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fechaLimite` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fechaDevolucion` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `tipoPrestamo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idPersona` int(11) NOT NULL,
+  `identificacion` varchar(50) NOT NULL,
+  `nombre1` varchar(50) NOT NULL,
+  `nombre2` varchar(50) NOT NULL,
+  `apellido1` varchar(50) NOT NULL,
+  `apellido2` varchar(50) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `sexo` varchar(50) NOT NULL,
+  `correo` varchar(60) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `tipoUsuario` varchar(50) NOT NULL,
+  `inscrito` varchar(50) NOT NULL,
+  `codigoInstitucional` varchar(50) NOT NULL,
+  `pagoAnual` double NOT NULL,
+  `controlPago` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Índices para tablas volcadas
 --
@@ -54,6 +132,30 @@ ALTER TABLE `funcionario`
   ADD PRIMARY KEY (`idPersona`);
 
 --
+-- Indices de la tabla `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`idMaterial`);
+
+--
+-- Indices de la tabla `multa`
+--
+ALTER TABLE `multa`
+  ADD PRIMARY KEY (`idMulta`);
+
+--
+-- Indices de la tabla `prestamo`
+--
+ALTER TABLE `prestamo`
+  ADD PRIMARY KEY (`idPrestamo`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idPersona`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -61,6 +163,30 @@ ALTER TABLE `funcionario`
 -- AUTO_INCREMENT de la tabla `funcionario`
 --
 ALTER TABLE `funcionario`
+  MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `material`
+--
+ALTER TABLE `material`
+  MODIFY `idMaterial` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `multa`
+--
+ALTER TABLE `multa`
+  MODIFY `idMulta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamo`
+--
+ALTER TABLE `prestamo`
+  MODIFY `idPrestamo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
   MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
