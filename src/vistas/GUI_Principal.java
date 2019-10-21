55ef05449919d116d2fa216e1d8f7d7041aabe49
Esteven Javier Mc <EstevenJaviier@gmail.com>
2019-10-21 14:58:16 -0500

    deleted class principal and conf style jform

--- src/vistas/GUI_Principal.java
+++ src/vistas/GUI_Principal.java
@@ -731,9 +731,9 @@ public class GUI_Principal extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
+                   break;
                 }
             }
         } catch (ClassNotFoundException ex) {
